50465d83aa893fdc3258a9a5d2ffc6a3cf66ffd2
Vinicius Carvalho de Souza <viniciuscarvalhomuay@gmail.com>
2023-08-24 21:44:53 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 function faq() {
   const dt = document.querySelectorAll('dt');
+  const dd = document.querySelectorAll('dd');
+  dd[0].classList.add('ativo');
   dt.forEach((item) => {
     item.addEventListener('click', faqItem);
   });
 
   function faqItem() {
-    this.nextElementSibling.classList.toggle('ativo');
+    dd.forEach((item) => {
+      item.classList.remove('ativo');
+    });
+    this.nextElementSibling.classList.add('ativo');
     this.classList.toggle('ativo');
   }
 }
